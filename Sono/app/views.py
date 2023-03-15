2cc9923c99adcfbc3f3ae950ac5e0e87db8885f2
martarozpondek <martarozpondek@wp.pl>
2023-03-15 18:34:46 +0100

    Create some basic views.

--- Sono/app/views.py
+++ Sono/app/views.py
@@ -1,2 +1,36 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(
+        request,
+        'app/home.html'
+        )
+
+def signin(request):
+    return render(
+        request,
+        'app/signin.html'
+        )
+
+def signup(request):
+    return render(
+        request,
+        'app/signup.html'
+        )
+
+def catalog(request):
+    return render(
+        request,
+        'app/catalog.html'
+        )
+
+def about(request):
+    return render(
+        request,
+        'app/about.html'
+        )
+def contact(request):
+    return render(
+        request,
+        'app/contact.html'
+        )
