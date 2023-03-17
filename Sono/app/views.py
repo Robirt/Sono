f998d8b7445146c64e7c71c977f72edaca254c0e
martarozpondek <martarozpondek@wp.pl>
2023-03-17 20:45:34 +0100

    Add basic of actions: sign_in, sign_out, sign_up.

--- Sono/app/views.py
+++ Sono/app/views.py
@@ -1,2 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
+def sign_in(request):
+    if request.method == 'POST':
+        user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('')
+        else:
+            return render(request, 'signin.html', {'error_message':'Nieprawid³owy login lub has³o'})
+    else:
+        return render(request, 'signin.html')
+
+def sign_out(request):
+    logout(request)
+    return redirect('')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sign_in')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
